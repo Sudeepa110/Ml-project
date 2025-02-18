b2266e2e2b957a2b460613896af29d342508a840
Sudeepa110 <sudeepa.rajesh1102@gmail.com>
2025-02-19 01:49:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,21 @@ import streamlit as st
 import numpy as np
 
 
-model = "traffic_sign_model.h5"
+MODEL_PATH = "traffic_sign_model.h5"
 url = "https://drive.google.com/file/d/1I5QMX2hgGvIEKcHbqHFZ31R5XjE1Sr5c"
 
 # Download the model
-gdown.download(url, model, fuzzy=True, quiet=False)
+gdown.download(url, MODEL_PATH, fuzzy=True, quiet=False)
 
 # Check if the file exists and is valid
-if not os.path.exists(model):
-    raise FileNotFoundError(f"Model file not found at {model}")
+if not os.path.exists(MODEL_PATH):
+    raise FileNotFoundError(f"Model file not found at {MODEL_PATH}")
 else:
-    print(f"Model file exists: {model}")
+    print(f"Model file exists: {MODEL_PATH}")
 
 # Try loading the model
 try:
-    model1 = load_model(model)
+    model = load_model(MODEL_PATH)
     print("Model loaded successfully!")
 except Exception as e:
     print(f"Error loading model: {e}")
