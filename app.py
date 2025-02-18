81988fa746a8d0bb9609c97ef63fc68169f9f62d
Sudeepa110 <sudeepa.rajesh1102@gmail.com>
2025-02-19 01:30:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 MODEL_PATH = "traffic_sign_model.h5"
 
 if not os.path.exists(MODEL_PATH):
-    url = "https://drive.google.com/file/d/1I5QMX2hgGvIEKcHbqHFZ31R5XjE1Sr5c/view?usp=sharing"  # Replace with the direct download link
+    url = "https://drive.google.com/uc?id=1I5QMX2hgGvIEKcHbqHFZ31R5XjE1Sr5c"  # Replace with the direct download link
     gdown.download(url, MODEL_PATH, quiet=False)
 
 model = load_model(MODEL_PATH)
