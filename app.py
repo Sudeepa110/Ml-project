04a2e966b8ce2b53ed0f3b8dd1eb4e27e2b7b59e
Sudeepa110 <sudeepa.rajesh1102@gmail.com>
2025-02-19 01:46:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ model = "traffic_sign_model.h5"
 url = "https://drive.google.com/file/d/1I5QMX2hgGvIEKcHbqHFZ31R5XjE1Sr5c"
 
 # Download the model
-gdown.download(url, MODEL_PATH, fuzzy=True, quiet=False)
+gdown.download(url, model, fuzzy=True, quiet=False)
 
 # Check if the file exists and is valid
-if not os.path.exists(MODEL_PATH):
-    raise FileNotFoundError(f"Model file not found at {MODEL_PATH}")
+if not os.path.exists(model):
+    raise FileNotFoundError(f"Model file not found at {model}")
 else:
-    print(f"Model file exists: {MODEL_PATH}")
+    print(f"Model file exists: {model}")
 
 # Try loading the model
 try:
-    model = load_model(MODEL_PATH)
+    model1 = load_model(model)
     print("Model loaded successfully!")
 except Exception as e:
     print(f"Error loading model: {e}")
