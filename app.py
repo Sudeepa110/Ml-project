7dbf4aa045ba6decf435d28d9cfae3fa1d94d6e3
Sudeepa110 <sudeepa.rajesh1102@gmail.com>
2025-02-19 01:24:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import gdown  # Install using `pip install gdown` for Google Drive downloads
+import os
+from tensorflow.keras.models import load_model
+
+MODEL_PATH = "traffic_sign_model.h5"
+
+if not os.path.exists(MODEL_PATH):
+    url = "https://drive.google.com/file/d/1I5QMX2hgGvIEKcHbqHFZ31R5XjE1Sr5c/view?usp=sharing"  # Replace with the direct download link
+    gdown.download(url, MODEL_PATH, quiet=False)
+
+model = load_model(MODEL_PATH)
